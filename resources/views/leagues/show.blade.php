5a2d36f4497924bad3dbbfa360a11d1e15902e5d
MauDesign <152592908+MauDesign@users.noreply.github.com>
2024-10-01 09:04:02 -0600

    show playeres add

--- resources/views/leagues/show.blade.php
+++ resources/views/leagues/show.blade.php
@@ -3,10 +3,22 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
            Liga {{ $league->name }}
         </h2>
+        <h4>Fecha de inicio: {{ $league->start_date}}</h4>
+            <h4>Jugadores participantes: {{ $league->players}}</h4>
+            <h4>Jornadas: {{ $league->rounds}}</h4>
+        <div>
+            <form action="" method="POST">
+                @csrf
+                @method('DELETE')
+                @include('players.form-creat-players')
+                <button type="submit" class="btn btn-danger">Crear jugador</button>
+            </form>
+        </div>
         <div class="filters align-content-end">
             <div class="col-m6"></div>
             <a href="{{route('leagues.create')}}" class="btn btn-primary" >Agregar</a>
         </div>
+
         <div class="card-datatable table-responsive pt-0 mt-4 mb-4">
             <table class="datatables-basic table table-bordered">
                 <thead>
