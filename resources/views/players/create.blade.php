f71c99ae9277768ec8e585d2c57f405f9be670e4
Yahir David Ramirez Vazquez <yahir.ramirez737@gmail.com>
2024-12-20 13:47:24 -0600

    Se soluciona error en vista Creacion Jugadores

--- resources/views/players/create.blade.php
+++ resources/views/players/create.blade.php
@@ -2,9 +2,9 @@
 <div class="container card pb-3">
     <form action="{{ route('leagues.store') }}" method="POST">
         @csrf
-        <h1>Crear liga</h1>
+        <h1>Crear Jugador</h1>
 
-        @include('leagues.form-creat-players')
+        @include('players.form-creat-players')
         <div class="mt-4 align-content-end">
             <a href="{{route('leagues.index')}}"  class="btn btn-outline-secondary" >Cancel</a>
             <button type="submit" class="btn btn-primary">Agregar</button>
